eac12f028caaefdbf873eba7d997bc776dd7528e
namdhev.ganeshan <namdhev.ganeshan@thoughtworks.com>
2024-08-30 10:33:48 +0530

    Fix trial for the eCharts width/height issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { TracingComponent } from './modules/tracing/tracing.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
   { path: 'tracing', component: TracingComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
 ]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes, { useHash: true })
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   providers: [],
